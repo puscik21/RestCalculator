a5c3584863d6590f6e9f222c4bb55beac4d0d08c
Grzegorz.Puscizna <grzesiekpuscizna@gmail.com>
2021-05-16 11:48:14 +0200

    Initial commit of Spring boot

---
+++ src/main/java/com/silvair/demo/RestCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.silvair.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestCalculatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/silvair/demo/RestCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silvair.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
